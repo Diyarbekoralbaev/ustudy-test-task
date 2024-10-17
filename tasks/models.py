cdd06c23e8bbbfb9be04ed4f542a16129440c316
Diyarbek <diyarbekdev@gmail.com>
2024-10-17 15:43:10 +0500

    PriorityChoiceField created

--- tasks/models.py
+++ tasks/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from users.models import UserModel
 
 
+class PriorityChoiceField(models.TextChoices):
+    LOW = 'low', 'Low'
+    MEDIUM = 'medium', 'Medium'
+    HIGH = 'high', 'High'
+
 class TaskModel(models.Model):
     STATUS_CHOICES = [
         ('new', 'New'),
@@ -11,6 +16,7 @@ class TaskModel(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    priority = models.CharField(max_length=20, choices=PriorityChoiceField.choices, default=PriorityChoiceField.LOW)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
     deadline = models.DateTimeField()
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='tasks')
